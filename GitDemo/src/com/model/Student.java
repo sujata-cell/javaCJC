dea7398cff33471916e6c937a73a65d19f476d94
sujata-cell <76507092+sujata-cell@users.noreply.github.com>
2024-11-12 16:08:38 +0530

    Update Student.java

--- GitDemo/src/com/model/Student.java
+++ GitDemo/src/com/model/Student.java
@@ -2,6 +2,10 @@ package com.model;
 
 public class Student {
 
+	public String m1()
+	{
+		return "This is m1 method";
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
