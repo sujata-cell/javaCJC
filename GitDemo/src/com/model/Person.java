172b9610da90a693eac02a8adf2bc8b6fbe02c3b
sujata-cell <76507092+sujata-cell@users.noreply.github.com>
2024-11-12 16:27:44 +0530

    Update Person.java

--- GitDemo/src/com/model/Person.java
+++ GitDemo/src/com/model/Person.java
@@ -6,7 +6,10 @@ public class Person {
 	{
 		return "method mm";
 	}
-	
+	public String mm1()
+	{
+		return "method mm 1";
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HII");
