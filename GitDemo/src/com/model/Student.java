ba05d9f3435e43ab9afab5b4f25af420d1764e46
sujata <sujata@DESKTOP-HNCHRD1>
2024-11-12 16:15:07 +0530

    m change

--- GitDemo/src/com/model/Student.java
+++ GitDemo/src/com/model/Student.java
@@ -2,6 +2,11 @@ package com.model;
 
 public class Student {
 
+	public String m()
+	{
+		return "okk"; 
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hi");
